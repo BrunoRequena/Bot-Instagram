f7e008e1665a9667d9ebed4a2f2cdf015a52defe
BrunoRequena <brunorequena15@hotmail.com>
2023-01-24 02:16:46 -0300

    First Commit

--- apps/bot_instagram.py
+++ apps/bot_instagram.py
@@ -18,11 +18,9 @@ class InstaFollower:
         self.driver = webdriver.Chrome(service=servico)
 
     def login(self, url, email, password):
-        #   open instagram
         self.driver.get(url=url)
         sleep(5)
 
-        #   login
         email_input = self.driver.find_element(By.NAME, "username")
         email_input.send_keys(email)
 
@@ -33,19 +31,16 @@ class InstaFollower:
         sleep(5)
 
     def find_followers(self, url, account):
-        #   get profile
         sleep(5)
         self.driver.get(url=f"{url}{account}")
         sleep(10)
 
-        #   click on followers
         followers = self.driver.find_element("xpath", '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a/div')
         followers.click()
         sleep(15)
 
         modal = self.driver.find_element("xpath", '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
 
-        # while True:
         for _ in range(2):
             sleep(5)
             self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
@@ -57,7 +52,6 @@ class InstaFollower:
     def follow(self):
         print("Follow start")
         sleep(5)
-        # buttons = self.driver.find_elements(By.PARTIAL_LINK_TEXT, "Seguir")
 
         buttons = self.driver.find_elements(By.CSS_SELECTOR, "._aano div div div button")
 
@@ -66,18 +60,15 @@ class InstaFollower:
         for button in buttons:
             button.click()
             print(button.text)
-            sleep(2)
+            sleep(3)
 
         print("Follow end")
 
 
 bot = InstaFollower(driver=servico)
 
-#   login instagram
 bot.login(url=INSTAGRAM_URL, email=EMAIL, password=PASSWORD)
 
-#   find followers
 bot.find_followers(url=INSTAGRAM_URL, account=USERNAME)
 
-#   follow
 bot.follow()
